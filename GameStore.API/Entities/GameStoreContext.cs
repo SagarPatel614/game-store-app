e3223f360510b86117e47e4a9123dd79c4e8e142
Sagar <scp1406@gmail.com>
2024-07-30 17:59:58 -0400

    data: Seed genres on startup

--- GameStore.API/Entities/GameStoreContext.cs
+++ GameStore.API/Entities/GameStoreContext.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.Common;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.API.Entities;
 
@@ -9,5 +8,16 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options)
     public DbSet<Game> Games => Set<Game>();
 
     public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Fighting" },
+            new { Id = 2, Name = "Roleplaying" },
+            new { Id = 3, Name = "Sports" },
+            new { Id = 4, Name = "Racing" },
+            new { Id = 5, Name = "Kids and Family" }
+        );
+    }
 
 }
